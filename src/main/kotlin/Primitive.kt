61defa5b2fd66c04634e009dd56ed8184515e3bd
Joel James <JamesJ@b2b.regn.net>
2022-10-20 13:54:51 -0700

    more

--- src/main/kotlin/Primitive.kt
+++ src/main/kotlin/Primitive.kt
@@ -1,11 +1,17 @@
 fun main(args: Array<String>) {
-    val b1: Byte  = 127
-    val b2: Short  = 100
-    val b3: Int  = 799
-    val bl: Long  = 7000 // or val b = 7000L
-    val bf: Float  = 3.7f //You need the lowercase f, because by default Kotlin stores decimals in doubles
+    val b1: Byte = 127
+    val s: Short = 100
+    val bi: Int = 799
+    val i = 799
+    val bl: Long = 7000
+    val l = 7000L
+    val bf: Float = 3.7f //You need the lowercase f, because by default Kotlin stores decimals in doubles
+    val f = 3.7f
+    //Double is the default datatype in Kotlin
     val bd: Double = 3.56 // or val d  = 3.723
-    val bc: Char  = 'K' // we have to use single quotes
+    val d = 3.56
+    val bc: Char = 'K' // we have to use single quotes
+    val c = 'K' // we have to use single quotes
 
-    //All the above datatypes has null safety. They cannot store null as a value.
+    //All the above data types has null safety. They cannot store null as a value.
 }
