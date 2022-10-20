cf50eb830bee39291f72f59487bc49f8561035e5
Joel James <JamesJ@b2b.regn.net>
2022-10-20 13:27:12 -0700

    Add raw string

---
+++ src/main/kotlin/RawStrings.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    val multiLine = """This is the first line.
+        This is the second.
+        This is the third.
+    """.trimIndent()
+
+    println("With trim indent $multiLine")
+
+    val fill = "Some message"
+    val multiLineTrimMargin = """This is the first line. $fill
+        |This is the second.
+        |This is the third.
+    """.trimMargin()
+
+    println("With trim margin $multiLineTrimMargin")
+}
