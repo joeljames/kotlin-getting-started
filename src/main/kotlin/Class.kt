5f2169b7d078143bef9f0b8832feea1af45faa5c
Joel James <JamesJ@b2b.regn.net>
2022-10-21 14:42:07 -0700

    Data Class

--- src/main/kotlin/Class.kt
+++ src/main/kotlin/Class.kt
@@ -49,17 +49,47 @@ class Emp(val name: String, var age: Int, val address: String = "default address
     }
 }
 
+
+//===========
+//Constant
+//===========
+//You don't need a class to define a constant
+val MY_CONSTANT = 100
+
+//===========
+//Data Class
+//===========
+//It comes with nice toString function, equals, hashCode, copy
+//data classes cannot be abstract, sealed or inner class
+data class Car(val color: String, val model: String, val year: Int) {
+
+}
+
 fun main(args: Array<String>) {
+    println(MY_CONSTANT)
     val emp1 = Emp(name = "JJ", age = 20, address = "Some Address")
     println(emp1.age)
-    emp1.age= 35
+    emp1.age = 35
     println(emp1.age)
     println(emp1)
 
     val emp2 = Emp("KK", 20, "Address 2")
     println(emp2)
-
     val emp3 = Emp("LL", 50)
     println(emp3)
+
+    println("Data Class")
+    val car = Car("Pink", "Honda", 2022)
+    println(car)
+    val car2 = Car("Pink", "Honda", 2022)
+    println(car == car2)
+    val car3 = car.copy()
+    println(car3)
+    val car4 = car.copy(year = 2016)
+    println(car4)
+    val car5 = car.copy(year = 2016, color = "Yellow")
+    println(car5)
+
+
 
 }
