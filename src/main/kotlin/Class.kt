4b35e229257c961f12e89670f8adb5bb2b73906e
Joel James <JamesJ@b2b.regn.net>
2022-10-21 14:17:22 -0700

    more

--- src/main/kotlin/Class.kt
+++ src/main/kotlin/Class.kt
@@ -8,12 +8,34 @@
 //protected -> Can't be used
 //public -> Visible to everyone
 //internal - Visible within the same module
-private class MyPrivateClass () {
+private class MyPrivateClass() {
+
+}
+
+//Protected constructor then you have to include the word constructor
+class EmpProtected protected constructor(val name: String) {}
+
+
+//Most of the time you won't need the secondary constructor.
+//You could just pass in default value to the properties.
+class EmpPrimaryAndSecondaryConstructor(val name: String) {
+
+    var fullTime: Boolean = true
+
+    //Secondary Constructor
+    constructor(name: String, fullTime: Boolean) : this(name) {
+        this.fullTime = fullTime
+    }
+}
+
+
+class EmpPrivateProperties(private val name: String) {
 
 }
 
 class Emp(val name: String, var age: Int, val address: String = "default address") {
 
+    //Not a constructor. Run when the instance is initialized.
     init {
         println("This is the class initializer")
     }
@@ -28,13 +50,16 @@ class Emp(val name: String, var age: Int, val address: String = "default address
 }
 
 fun main(args: Array<String>) {
-    val emp1 = Emp(name="JJ", age=20, address = "Some Address")
+    val emp1 = Emp(name = "JJ", age = 20, address = "Some Address")
+    println(emp1.age)
+    emp1.age= 35
+    println(emp1.age)
     println(emp1)
 
     val emp2 = Emp("KK", 20, "Address 2")
     println(emp2)
 
-    val emp3 = Emp("LL", 50,)
+    val emp3 = Emp("LL", 50)
     println(emp3)
 
 }
