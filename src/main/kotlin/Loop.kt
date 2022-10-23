2c4f13ae5b59701632425724bf0d071c7f056280
Joel James <JamesJ@b2b.regn.net>
2022-10-22 20:33:06 -0700

    loops

--- src/main/kotlin/Loop.kt
+++ src/main/kotlin/Loop.kt
@@ -1,22 +1,64 @@
 fun main(args: Array<String>) {
+    //=========
     //For Loop
-    //This will print form 1 to 10
-    for (i in 1..10) {
+    //=========
+    //Range is inclusive. Includes 1 and 5.
+    //Use until if you don't want to include the end
+    val range = 1..5
+    val charRange = 'a'..'z'
+    val strRange = "ABC".."XYZ"
+
+    println(3 in range)
+    println('x' in charRange)
+    println("CCC" in strRange)
+    println("CCCC" in strRange)
+
+    val backwardRange = 5.downTo(1)
+
+    val stepRage = 3..15
+    val stepThree = stepRage.step(3)
+    println("stepThree")
+    for (i in stepThree) {
         println(i)
     }
 
-    println("Printing in opposite order")
-    for (i in 10 downTo 0) {
+    println("until excludes the last from the range")
+    for (i in 1 until 5) {
         println(i)
     }
 
+    println("Inline step")
+    for (i in 1..15 step 2) {
+        println(i)
+    }
+
+    println("Inline down to")
+    for (i in 20 downTo 15) {
+        println(i)
+    }
+
+    val str = "My String"
+    for (i in str) {
+        println(i)
+    }
+
+    val seasons = arrayOf("spring", "fall")
+    for (i in seasons) {
+        println(i)
+    }
+    for (i in seasons.indices) {
+        println(seasons[i])
+    }
+    //lambda
+    seasons.forEach { println(it) }
+    seasons.forEachIndexed { index, value -> println("$value is indexed at $index") }
+
+    println("abc" in seasons)
+    println("abc" !in seasons)
+    println("e" in "abc")
 
     //Loop through an array
     val lan = listOf("Python", "Java", "Kotlin")
-    for (elements in lan) {
-        println(elements)
-    }
-
     for ((index, value) in lan.withIndex()) {
         println("Element at $index is value $value")
     }
