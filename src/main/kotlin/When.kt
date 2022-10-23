6cbe232dd6c4e25be4802121cf734e97ca6ce200
Joel James <JamesJ@b2b.regn.net>
2022-10-22 20:51:44 -0700

    when

--- src/main/kotlin/When.kt
+++ src/main/kotlin/When.kt
@@ -1,11 +1,15 @@
 fun main(args: Array<String>) {
-    val mode: Int = 2
+    val mode: Int = 1
     when(mode) {
-        1 -> println("The mode is 1")
+        0, 1 -> {
+            println("The mode is either 0 or 1")
+
+        }
         2 -> {
             println("The mode is 2")
             println("The mode is busy")
         }
+//        is Int -> println("is an instance of int")
         in 1..10 -> println("mode is between 1 and 10")
         !in 20..25 -> println("mode is not between 20 and 25")
         else -> println("I don't know that mode")
