04c38b81243701768ffd482b031780814327c674
jjames <jjames@newrelic.com>
2022-11-08 11:40:33 -0800

    format code

--- src/main/kotlin/NullReference.kt
+++ src/main/kotlin/NullReference.kt
@@ -1,43 +1,35 @@
 fun main(args: Array<String>) {
-
-    //========
-    //?
-    //========
-    //Variable can be nullable by using ?
+    // ========
+    // ?
+    // ========
+    // Variable can be nullable by using ?
     val someString: String? = null
-    //No NPE is raised. Since we are using the safety operator
+    // No NPE is raised. Since we are using the safety operator
     println("someString: ${someString?.uppercase()}")
 
-    //========
-    //?: elvis operator of Kotlin
-    //========
-    //It lest you assign a default value when an expression evaluates to null
-    val newString =  someString?.uppercase() ?: "THIS IS THE DEFAULT"
+    // ========
+    // ?: elvis operator of Kotlin
+    // ========
+    // It lest you assign a default value when an expression evaluates to null
+    val newString = someString?.uppercase() ?: "THIS IS THE DEFAULT"
     println("The elvis operator example $newString")
 
-    //========
-    //!! (assert not null)
-    //========
+    // ========
+    // !! (assert not null)
+    // ========
     val someString2: String? = "This is not null"
-    //!! will a NPE if someString2 is null. Use it if you want NPE to be thrown.
+    // !! will a NPE if someString2 is null. Use it if you want NPE to be thrown.
     println("someString2:  ${someString2!!.uppercase()}")
 
-    //========
-    //let
-    //========
+    // ========
+    // let
+    // ========
     val someString3: String? = "This is not null"
 //    plainText(someString3) //You cannot do this, the fun requires a nun nullable string
-    //The below will work, but not a recommended way
+    // The below will work, but not a recommended way
     plainText(someString3!!)
-    //Use the let function instead(Shorthand). Tis is same as if (someString3 != null) {plainText(someString3)}
+    // Use the let function instead(Shorthand). Tis is same as if (someString3 != null) {plainText(someString3)}
     someString3?.let { plainText(it) }
-
-
-
-
-
-
-
 }
 
 fun plainText(text: String) {
