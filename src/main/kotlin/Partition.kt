326315e72fce1904f82fe7d81e2a4095e0961d4e
jjames <jjames@newrelic.com>
2023-04-04 09:41:05 -0700

    Partition into equal chunked bucket.

---
+++ src/main/kotlin/Partition.kt
@@ -0,0 +1,35 @@
+fun main() { // ktlint-disable filename
+    //
+    println("Partition int.max an int.min into equal buckets.")
+    val min = Int.MIN_VALUE
+    val max = Int.MAX_VALUE
+    val range = max.toLong() - min.toLong()
+    val partition = 3
+
+    // Returns a sequence of Pair for each bucket.
+    val partitionedBucketSequence = (1..3).asSequence().map {
+        when (it) {
+            // First bucket
+            1 -> {
+                val first = min
+                val second = (min.toLong() + (it * range / partition)).toInt()
+                first to second
+            }
+            // Last bucket
+            partition -> {
+                val first = (min.toLong() + ((partition - 1) * range / partition)).toInt()
+                val second = max
+                first to second
+            }
+            // Intermediate bucket
+            else -> {
+                val first = (min.toLong() + ((it - 1) * range / partition)).toInt()
+                val second = (min.toLong() + (it * range / partition)).toInt()
+                first to second
+            }
+        }
+    }
+    partitionedBucketSequence.forEach {
+        println("First: ${it.first}, Second: ${it.second}")
+    }
+}
